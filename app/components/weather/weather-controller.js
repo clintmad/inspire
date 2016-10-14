37d1e98960faa165ccb2804c40235425f5837fa6
Clint Madariaga <clintmad21@hotmail.com>
2016-10-14 17:09:57 -0600

    Toggle!

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -1,51 +1,45 @@
-(function (){
-	
+(function () {
+
 	var wc = this;
 	var weatherService = new WeatherService();
-	
+
 	weatherService.getWeather(updateWeather)
-		
-		
-		
-	function updateWeather(data){
+
+
+
+	function updateWeather(data) {
 		var results = JSON.parse(data)
 		var weatherElem = $('#weather')
 		var tempK = results.main.temp
-		var tempF = tempK * (9/5) -459.67
-		var tempC = (tempF - 32) * (5/9)
-		tempC= Math.ceil(tempC)
-		tempF= Math.ceil(tempF)
+		var tempF = tempK * (9 / 5) - 459.67
+		var tempC = (tempF - 32) * (5 / 9)
+		tempC = Math.ceil(tempC)
+		tempF = Math.ceil(tempF)
 		var weatherTemplate = '';
 		weatherTemplate += `
 		<div class="card">
 			<h2>${results.name}</h2>
-			
-			
+			<span id="c-temp">${tempC}° C</span><span id="f-temp">${tempF}° F</span>			
 			<img src="http://openweathermap.org/img/w/${results.weather[0].icon}.png">
 			<h3>${results.weather[0].description}</h3>
-			<button type="btn" id="buttonC"><h1 id="c">${tempC} C</h1></button>
-			<button type="btn" id="buttonF"><h1 id ="f">${tempF} F</h1></button>		
+					
 		</div>
 		
 		`
 		weatherElem.append(weatherTemplate)
-		$('#buttonC').hide();
 
-		$('#buttonC').on('click', function(e){
-			e.preventDefault();
-			$('#buttonF').hide();
-			$('#buttonC').show();
+		$('#c-temp').hide();
+
+		$('#weather').on('click', function () {
+			$('#c-temp').toggle();
+			$('#f-temp').toggle();
 		})
 
-		$('#buttonF').on('click', function(e){
-			e.preventDefault();
-			$('#buttonC').hide();
-			$('#buttonF').show();
-		})
+
 	}
-	
-	
-	
-	
-	
-}())
+
+
+
+
+
+} ())
